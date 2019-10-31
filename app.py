15a2f9b7aa3109f898408838c7db490ad6ba165f
Flavio Giobergia <flavio.giobergia@gmail.com>
2019-10-31 17:45:53 +0100

    added initial evaluation

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import traceback
 
-from flask import Flask, send_file, session, jsonify, redirect, flash, url_for
+from flask import Flask, send_file, session, jsonify, redirect, flash, url_for, request
 from flask import render_template, request
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import competition_tools
 import os
 from api_utils import ApiAuth
+from models import db, Submission, Evaluation
+from competition_tools import eval_public_private
 
 #TODO Load configuration from config.yaml
 UPLOAD_FOLDER = './uploads'
 TEST_FILE_PATH = './static/test_solution/test_solution.csv'
 MAX_FILE_SIZE = 32 * 1024 * 1024  # limit upload file size to 32MB
 API_FILE = 'mappings.dummy.json'
+DB_FILE = 'sqlite:///test.db'
 
 
 app = Flask(__name__, static_url_path="", static_folder="static")
@@ -23,6 +26,10 @@ app.secret_key = os.urandom(24)
 CORS(app)
 
 api_auth = ApiAuth(API_FILE)
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_FILE
+db.init_app(app)
+db.app = app
+db.create_all()
 
 ################
 # Error Handling
@@ -47,21 +54,30 @@ def error():
 @app.route('/evaluate', methods=["GET"])
 def evaluate():
     try:
-        if ("submit_request_id" not in session.keys()) or \
-                (session["submit_request_id"] != request.args.get('submitRequestId', None)):
-            error_message = "Wrong request. Use the form web page to upload a solution or try to reload the page!"
-            raise Exception(error_message)
+        api_key = request.args.get("api_key")
+        submission_id = request.args.get("submission_id")
+        print(api_key, submission_id)
+        if not api_auth.is_valid(api_key):
+            # TODO build dictionary of possible errors & avoid hardcoding strings
+            raise Exception("Invalid API key!")
 
-        submitted_to_evaluate = request.args["submitted_to_evaluate"]
-
-        # TODO run evaluation here
+        user_id = api_auth.get_user(api_key)
+        print(user_id)
+        submission = Submission.query.filter_by(id=submission_id, user_id=user_id).first()
+        public_score, private_score = eval_public_private(submission.filename, TEST_FILE_PATH)
+        if not submission:
+            # not found!
+            raise Exception("Submission not found!")
 
     except Exception as ex:
         traceback.print_stack()
         traceback.print_exc()
         return redirect(url_for('error', error_message=ex))
 
-    return jsonify(submitted_to_evaluate)
+    evaluation = Evaluation(submission=submission, evaluation_public=public_score, evaluation_private=private_score)
+    db.session.add(evaluation)
+    db.session.commit()
+    return jsonify({ "score": public_score })
 
 
 ################
@@ -83,6 +99,7 @@ def upload():
         api_key = request.form.get("APIKey", None)
         if not api_auth.is_valid(api_key):
             raise Exception("Invalid API key!")
+        user_id = api_auth.get_user(api_key) # This will be stored in the Submissions table
 
         # Save submitted solution
         if request.method == 'POST':
@@ -107,11 +124,17 @@ def upload():
 
             elif file:
                 timestamp = competition_tools.get_timestamp()
-                new_file_name = f"{timestamp}_{api_key}.csv"
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_file_name))
-                return redirect(url_for('evaluate',
-                                        submitted_to_evaluate=new_file_name,
-                                        submitRequestId=session["submit_request_id"]))
+                new_file_name = f"{timestamp}_{user_id}.csv"
+                output_file = os.path.join(app.config['UPLOAD_FOLDER'], new_file_name)
+                # we are reading the stream when checking the file, so we need to go back to the start
+                file.stream.seek(0)
+                file.save(output_file)
+                submission = Submission(user_id=user_id, filename=output_file)
+                db.session.add(submission)
+                db.session.commit()
+                # By passing api_key, we can later check that the user calling /evaluate
+                # is the same that has made the submission
+                return redirect(url_for('evaluate', submission_id=submission.id, api_key=api_key))
             else:
                 raise Exception("You should not be here!")
     except Exception as ex:
--- competition_tools.py
+++ competition_tools.py
@@ -7,6 +7,9 @@ import os
 
 ALLOWED_EXTENSIONS = {'.csv'}
 HEADER = ["Id", "Predicted"]
+INDEX = "Id"
+TARGET = "Predicted"
+PUBLIC = "Public"
 
 def randomString(stringLength=8):
     """Generate a random string of fixed length """
@@ -15,15 +18,13 @@ def randomString(stringLength=8):
 
 
 def check_file(file, test_file):
-    check = True
-
     try:
-        test_df = pd.read_csv(test_file)
+        test_df = pd.read_csv(test_file, index_col=INDEX)
     except Exception as ex:
         raise Exception(f"Test solution error - File: {test_file} - {ex}")
 
-    submitted_df = pd.read_csv(file.stream)
-    submitted_columns = list(submitted_df.columns)
+    submitted_df = pd.read_csv(file.stream, index_col=INDEX)
+    submitted_columns = list(submitted_df.columns) + [INDEX] # "INDEX" is not included in .columns
     # check file schema
     if not (all([h in submitted_columns for h in HEADER]) == True):
         missing_cols = [h for h in HEADER if h not in submitted_columns]
@@ -36,10 +37,39 @@ def check_file(file, test_file):
     if len(submitted_df.index) != len(test_df.index):
         raise Exception(f"Submitted solution length does not match the dataset length. Submitted solution has {len(submitted_df.index)} rows while Dataset has {len(test_df.index)} rows.")
 
-    # check file size
+    # TODO: check file size
+
+    # check indices
+    if set(submitted_df.index) != set(test_df.index):
+        raise Exception("Indices do not match!")
 
     return True
 
+def eval_public_private(submission, solution):
+    try:
+        df_pred = pd.read_csv(submission, index_col=INDEX).sort_index()
+        df_true = pd.read_csv(solution, index_col=INDEX).sort_index()
+        assert len(df_pred) == len(df_true) # already checked, should be true!x
+        assert (df_pred.index == df_true.index).all() # already checked, should be true!
+    except Exception:
+        # We shuld never fail here -- the file has already been validated!
+        raise Exception("Unexpected error! Please contact an administrator")
+
+    # TODO implement custom function
+    def my_accuracy(y_true, y_pred):
+        return sum(1 for a,b in zip(y_true, y_pred) if a==b)/len(y_true)
+
+    public_mask = df_true[PUBLIC] == 1
+    y_pred_public = df_pred[public_mask][TARGET].values
+    y_true_public = df_true[public_mask][TARGET].values
+
+    y_pred_private = df_pred[~public_mask][TARGET].values
+    y_true_private = df_true[~public_mask][TARGET].values
+    
+    public_score = my_accuracy(y_true_public, y_pred_public)
+    private_score = my_accuracy(y_true_private, y_pred_private)
+    
+    return public_score, private_score
 
 def allowed_file(filename):
     return os.path.splitext(filename.lower())[1] in ALLOWED_EXTENSIONS
