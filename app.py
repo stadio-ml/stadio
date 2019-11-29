152cbc09bffef5d00afa07a4931adeeea4e893be
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2019-11-29 19:10:02 +0100

    Add a stage handler class. Extend configuration file with stages times. Add two templates for "ready" and "terminated" stages.

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 import secrets
 from api_utils import ApiAuth
 from models import db, Submission, Evaluation
-from competition_tools import eval_public_private
+from competition_tools import eval_public_private, StageHandler
 from sqlalchemy import func
 from datetime import datetime
 
@@ -26,12 +26,14 @@ TIME_BETWEEN_SUBMISSIONS = 5 * 60 # 5 minutes between submissions
 score_mapper = lambda score: f"{score * 100:.2f}"
 
 app = Flask(__name__, static_url_path="", static_folder="static")
+app.config.from_object("config.CompetitionConfig")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = MAX_FILE_SIZE
 app.secret_key = os.urandom(24)
 
 CORS(app)
 
+stage_handler = StageHandler(app.config['OPEN_TIME'], app.config['CLOSE_TIME'], app.config['TERMINATE_TIME'])
 api_auth = ApiAuth(API_FILE)
 app.config["SQLALCHEMY_DATABASE_URI"] = DB_FILE
 db.init_app(app)
@@ -60,54 +62,64 @@ def error():
 
 @app.route('/', methods=["GET"])
 def leaderboard():
-    # TODO: here, we assume that a higher score is preferable.
-    # it might not always be like this (e.g. MSE)
-    # For those cases, func.min should be used: make this parameter
-    # configurable from config file
-    participants = db.session \
-        .query(Submission.user_id, func.max(Evaluation.evaluation_public)) \
-        .join(Submission) \
-        .group_by(Submission.user_id) \
-        .order_by(Evaluation.evaluation_public.desc()) \
-        .all()
-    score = request.args.get("score")
-    highlight_user_id = request.args.get("highlight")
-    participants = [ (user_id, score_mapper(score)) for user_id, score in participants ]
-    if score:
-        try:
-            score = score_mapper(float(score))
-        except: # Just in case someone passes something nasty for `score`
-            score = None
-    return render_template("leaderboard.html", score=score, highlight_user_id=highlight_user_id, participants=participants)
+    if stage_handler.is_ready():
+        return render_template("ready.html", name=app.config['NAME'], open_time=stage_handler.open_time, close_time=stage_handler.close_time)
+    elif stage_handler.is_closed():
+        return render_template("over.html") # TODO freeze the final leaderboard and allow new submission
+    elif stage_handler.is_terminated(): # TODO handle this case
+        return render_template("over.html")
+    else:
+        # TODO: here, we assume that a higher score is preferable.
+        # it might not always be like this (e.g. MSE)
+        # For those cases, func.min should be used: make this parameter
+        # configurable from config file
+        participants = db.session \
+            .query(Submission.user_id, func.max(Evaluation.evaluation_public)) \
+            .join(Submission) \
+            .group_by(Submission.user_id) \
+            .order_by(Evaluation.evaluation_public.desc()) \
+            .all()
+        score = request.args.get("score")
+        highlight_user_id = request.args.get("highlight")
+        participants = [ (user_id, score_mapper(score)) for user_id, score in participants ]
+        if score:
+            try:
+                score = score_mapper(float(score))
+            except: # Just in case someone passes something nasty for `score`
+                score = None
+        return render_template("leaderboard.html", score=score, highlight_user_id=highlight_user_id, participants=participants)
 
 ################
 # Evaluate
 ################
 @app.route('/evaluate', methods=["GET"])
 def evaluate():
-    try:
-        api_key = request.args.get("api_key")
-        submission_id = request.args.get("submission_id")
-        if not api_auth.is_valid(api_key):
-            # TODO build dictionary of possible errors & avoid hardcoding strings
-            raise Exception("Invalid API key!")
+    if not stage_handler.can_submit():
+        redirect(url_for('leaderboard'))
+    else:
+        try:
+            api_key = request.args.get("api_key")
+            submission_id = request.args.get("submission_id")
+            if not api_auth.is_valid(api_key):
+                # TODO build dictionary of possible errors & avoid hardcoding strings
+                raise Exception("Invalid API key!")
 
-        user_id = api_auth.get_user(api_key)
-        submission = Submission.query.filter_by(id=submission_id, user_id=user_id).first()
-        public_score, private_score = eval_public_private(submission.filename, TEST_FILE_PATH)
-        if not submission:
-            # not found!
-            raise Exception("Submission not found!")
+            user_id = api_auth.get_user(api_key)
+            submission = Submission.query.filter_by(id=submission_id, user_id=user_id).first()
+            public_score, private_score = eval_public_private(submission.filename, TEST_FILE_PATH)
+            if not submission:
+                # not found!
+                raise Exception("Submission not found!")
 
-    except Exception as ex:
-        traceback.print_stack()
-        traceback.print_exc()
-        return redirect(url_for('error', error_message=ex))
+        except Exception as ex:
+            traceback.print_stack()
+            traceback.print_exc()
+            return redirect(url_for('error', error_message=ex))
 
-    evaluation = Evaluation(submission=submission, evaluation_public=public_score, evaluation_private=private_score)
-    db.session.add(evaluation)
-    db.session.commit()
-    return redirect(url_for('leaderboard', score=public_score, highlight=user_id))
+        evaluation = Evaluation(submission=submission, evaluation_public=public_score, evaluation_private=private_score)
+        db.session.add(evaluation)
+        db.session.commit()
+        return redirect(url_for('leaderboard', score=public_score, highlight=user_id))
 
 
 ################
@@ -115,65 +127,68 @@ def evaluate():
 ################
 @app.route('/upload', methods=["POST"])
 def upload():
-    error_message = ""
-    # Check submit request id
+    if not stage_handler.can_submit():
+        redirect(url_for("leaderboard"))
+    else:
+        error_message = ""
+        # Check submit request id
 
-    try:
-        if ("submit_request_id" not in session.keys()) or \
-                (session["submit_request_id"] != request.form.get('submitRequestId', None)):
-            error_message = "Wrong request. Use the form web page to upload a solution or try to reload the page!"
-            raise Exception(error_message)
-
-        api_key = request.form.get("APIKey", None)
-        if not api_auth.is_valid(api_key):
-            raise Exception("Invalid API key!")
-        user_id = api_auth.get_user(api_key)  # This will be stored in the Submissions table
-
-        # Save submitted solution
-        if request.method == 'POST':
-            latest_submission = db.session.query(func.max(Submission.timestamp)).filter(Submission.user_id == user_id).first()[0]
-            now = datetime.utcnow()
-            if latest_submission and (now - latest_submission).total_seconds() < TIME_BETWEEN_SUBMISSIONS:
-                delta = max(5, int(TIME_BETWEEN_SUBMISSIONS - (now - latest_submission).total_seconds())) # avoid messages such as "try again in 0/1/2 seconds" (TODO remove magic number 5)
-                raise Exception(f"You are exceeding the {TIME_BETWEEN_SUBMISSIONS} seconds limit between submissions. Please try again in {delta} seconds")
-            
-            # check if the post request has the file part
-            if 'submittedSolutionFile' not in request.files:
-                error_message = 'No file part'
+        try:
+            if ("submit_request_id" not in session.keys()) or \
+                    (session["submit_request_id"] != request.form.get('submitRequestId', None)):
+                error_message = "Wrong request. Use the form web page to upload a solution or try to reload the page!"
                 raise Exception(error_message)
 
-            file = request.files['submittedSolutionFile']
-            if not file:
-                error_message = 'Error uploading solution file.'
-                raise Exception(error_message)
+            api_key = request.form.get("APIKey", None)
+            if not api_auth.is_valid(api_key):
+                raise Exception("Invalid API key!")
+            user_id = api_auth.get_user(api_key)  # This will be stored in the Submissions table
 
-            # if user does not select file, browser also
-            # submit an empty part without filename
-            if file.filename == '':
-                error_message = 'No selected file'
-                raise Exception(error_message)
+            # Save submitted solution
+            if request.method == 'POST':
+                latest_submission = db.session.query(func.max(Submission.timestamp)).filter(Submission.user_id == user_id).first()[0]
+                now = datetime.utcnow()
+                if latest_submission and (now - latest_submission).total_seconds() < TIME_BETWEEN_SUBMISSIONS:
+                    delta = max(5, int(TIME_BETWEEN_SUBMISSIONS - (now - latest_submission).total_seconds())) # avoid messages such as "try again in 0/1/2 seconds" (TODO remove magic number 5)
+                    raise Exception(f"You are exceeding the {TIME_BETWEEN_SUBMISSIONS} seconds limit between submissions. Please try again in {delta} seconds")
+                
+                # check if the post request has the file part
+                if 'submittedSolutionFile' not in request.files:
+                    error_message = 'No file part'
+                    raise Exception(error_message)
 
-            if competition_tools.allowed_file(file.filename) and \
-                    competition_tools.check_file(file, TEST_FILE_PATH):
+                file = request.files['submittedSolutionFile']
+                if not file:
+                    error_message = 'Error uploading solution file.'
+                    raise Exception(error_message)
 
-                timestamp = competition_tools.get_timestamp()
-                new_file_name = f"{timestamp}_{user_id}.csv"
-                output_file = os.path.join(app.config['UPLOAD_FOLDER'], new_file_name)
-                # we are reading the stream when checking the file, so we need to go back to the start
-                file.stream.seek(0)
-                file.save(output_file)
-                submission = Submission(user_id=user_id, filename=output_file)
-                db.session.add(submission)
-                db.session.commit()
-                # By passing api_key, we can later check that the user calling /evaluate
-                # is the same that has made the submission
-                return redirect(url_for('evaluate', submission_id=submission.id, api_key=api_key))
-            else:
-                raise Exception("You should not be here!")
-    except Exception as ex:
-        traceback.print_stack()
-        traceback.print_exc()
-        return redirect(url_for('error', error_message=ex))
+                # if user does not select file, browser also
+                # submit an empty part without filename
+                if file.filename == '':
+                    error_message = 'No selected file'
+                    raise Exception(error_message)
+
+                if competition_tools.allowed_file(file.filename) and \
+                        competition_tools.check_file(file, TEST_FILE_PATH):
+
+                    timestamp = competition_tools.get_timestamp()
+                    new_file_name = f"{timestamp}_{user_id}.csv"
+                    output_file = os.path.join(app.config['UPLOAD_FOLDER'], new_file_name)
+                    # we are reading the stream when checking the file, so we need to go back to the start
+                    file.stream.seek(0)
+                    file.save(output_file)
+                    submission = Submission(user_id=user_id, filename=output_file)
+                    db.session.add(submission)
+                    db.session.commit()
+                    # By passing api_key, we can later check that the user calling /evaluate
+                    # is the same that has made the submission
+                    return redirect(url_for('evaluate', submission_id=submission.id, api_key=api_key))
+                else:
+                    raise Exception("You should not be here!")
+        except Exception as ex:
+            traceback.print_stack()
+            traceback.print_exc()
+            return redirect(url_for('error', error_message=ex))
 
 
 ################
@@ -181,9 +196,12 @@ def upload():
 ################
 @app.route('/submit', methods=["GET"])
 def submit():
-    submit_request_id = secrets.token_hex()
-    session["submit_request_id"] = submit_request_id
-    return render_template("submit.html", submit_request_id=submit_request_id)
+    if not stage_handler.can_submit():
+        redirect(url_for("leaderboard"))
+    else:
+        submit_request_id = secrets.token_hex()
+        session["submit_request_id"] = submit_request_id
+        return render_template("submit.html", submit_request_id=submit_request_id)
 
 
 if __name__ == '__main__':
---
+++ config.py
@@ -0,0 +1,7 @@
+class CompetitionConfig:
+    NAME = 'Lab #8'
+    
+    # UTC TIME
+    OPEN_TIME = '2019/11/29 18:09:00'
+    CLOSE_TIME = '2019/11/29 18:15:00'
+    TERMINATE_TIME = '2019/11/29 18:30:00'
