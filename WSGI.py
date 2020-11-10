a0cdf8a9a1407f998438b64af2edfec7b00163e4
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2020-11-10 11:49:45 +0100

    Add CL arguments to specify WSGI port and host.

--- WSGI.py
+++ WSGI.py
@@ -1,21 +1,40 @@
 import cherrypy as cherrypy
 from paste.translogger import TransLogger
 from app import app as flask_app
+import argparse
 
 
-app_logged = TransLogger(flask_app)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=8888,
+        help="The port of the WSGI server (default 8888)",
+    )
+    parser.add_argument(
+        "--host",
+        type=str,
+        default="0.0.0.0",
+        help="The hosts allowed by the WSGI server (default 0.0.0.0)",
+    )
+    args = parser.parse_args()
 
-# Mount the WSGI callable object (app) on the root directory
-cherrypy.tree.graft(app_logged, '/')
+    app_logged = TransLogger(flask_app)
 
-# Set the configuration of the web server
-cherrypy.config.update({
-    'engine.autoreload.on': False,
-    'log.screen': True,
-    'server.socket_port': 8080,
-    'server.socket_host': '0.0.0.0'
-})
+    # Mount the WSGI callable object (app) on the root directory
+    cherrypy.tree.graft(app_logged, "/")
 
-# Start the CherryPy WSGI web server
-cherrypy.engine.start()
-cherrypy.engine.block()
+    # Set the configuration of the web server
+    cherrypy.config.update(
+        {
+            "server.socket_port": args.port,
+            "server.socket_host": args.host,
+            "engine.autoreload.on": False,
+            "log.screen": True,
+        }
+    )
+
+    # Start the CherryPy WSGI web server
+    cherrypy.engine.start()
+    cherrypy.engine.block()
