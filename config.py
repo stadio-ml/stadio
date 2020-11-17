0872c2ff9424c412d19d68402d564040f897ca26
Giuseppe Attanasio <giuseppeattansio6@gmail.com>
2020-11-17 23:26:22 +0100

    Improve config class. Add a new BASE_DIR directory where all the configuration files and outputs of a single istance of the competiotion should be stored.

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+from os.path import join
+
+
 class CompetitionConfig:
     NAME = 'Lab #'
-    
+
     # UTC TIME (YYYY/MM/DD HH:MM:SS)
     OPEN_TIME = '2019/12/02 11:25:00'
     CLOSE_TIME = '2020/02/08 08:26:00'
@@ -10,12 +13,25 @@ class CompetitionConfig:
     ADMIN_USER_ID = "prof"
     BASELINE_USER_ID = "baseline"
 
-    UPLOAD_FOLDER = './uploads'  # Where to store submissions
-    DUMP_FOLDER = './dumps'  # Where to store DB dumps with scores
+    # BASE directory where you might want to store all the completition data
+    BASE_DIR = "base_dir"
 
-    TEST_FILE_PATH = './static/test_solution/test_solution.csv'  # './static/test_solution/eval_solution.csv'
+    # Folder in which every submission file is stored
+    UPLOAD_FOLDER = join(BASE_DIR, "uploads")
+
+    # Folder in which every database dump is stored
+    DUMP_FOLDER = join(BASE_DIR, "dumps")
+
+    # File with gold labels
+    TEST_FILE_PATH = join(BASE_DIR, "test_solution.csv")
+
     MAX_FILE_SIZE = 32 * 1024 * 1024  # limit upload file size to 32MB
-    API_FILE = 'mappings.dummy.json'  # API mappings
-    DB_FILE = 'sqlite:///test.db'
+
+    # File used to identify users on the platform
+    API_FILE = join(BASE_DIR, "mappings.dummy.json")  # API mappings
+
+    # The sqlite database for the current competition
+    DB_FILE = join(BASE_DIR, "sqlite:///test.db")
+
     TIME_BETWEEN_SUBMISSIONS = 5 * 60  # 5 minutes between submissions
     MAX_NUMBER_SUBMISSIONS = 100
