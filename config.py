0d6e9ee3d7b88a49f22720ad42f9b06cdbcf9521
Ven7u <francescoventura.183464@gmail.com>
2020-11-26 15:40:12 +0100

    update config.py

--- config.py
+++ config.py
@@ -1,21 +1,37 @@
+from os.path import join
+
+
 class CompetitionConfig:
-    NAME = 'Exam - February session'
-    
+    NAME = 'Lab #'
+
     # UTC TIME (YYYY/MM/DD HH:MM:SS)
     OPEN_TIME = '2019/12/02 11:25:00'
-    CLOSE_TIME = '2021/01/10 08:26:00'
-    TERMINATE_TIME = '2021/12/02 11:27:00'
+    CLOSE_TIME = '2020/02/08 08:26:00'
+    TERMINATE_TIME = '2020/12/02 11:27:00'
 
     # USERS with special behaviours
-    ADMIN_USER_ID = "prof"
+    ADMIN_USER_ID = "admin"
     BASELINE_USER_ID = "baseline"
 
-    UPLOAD_FOLDER = './uploads'  # Where to store submissions
-    DUMP_FOLDER = './dumps'  # Where to store DB dumps with scores
+    # BASE directory where you might want to store all the completition data
+    BASE_DIR = "base_dir"
 
-    TEST_FILE_PATH = './static/test_solution/test_solution.csv'  # './static/test_solution/eval_solution.csv'
+    # Folder in which every submission file is stored
+    UPLOAD_FOLDER = join(BASE_DIR, "uploads")
+
+    # Folder in which every database dump is stored
+    DUMP_FOLDER = join(BASE_DIR, "dumps")
+
+    # File with gold labels
+    TEST_FILE_PATH = join(BASE_DIR, "test_solution.csv")
+
     MAX_FILE_SIZE = 32 * 1024 * 1024  # limit upload file size to 32MB
-    API_FILE = '/Users/francescoventura/OneDrive - Politecnico di Torino/didattica/Data-Science-Lab/mappings.checkpoint.2019.2020.json' #'mappings.dummy.json'  # API mappings
-    DB_FILE = 'sqlite:///competition.lab9.db'
+
+    # File used to identify users on the platform
+    API_FILE = join(BASE_DIR, "mappings.dummy.tsv")  # API mappings
+
+    # The sqlite database for the current competition
+    DB_FILE = "sqlite:///" + join(BASE_DIR, "test.db")
+
     TIME_BETWEEN_SUBMISSIONS = 5 * 60  # 5 minutes between submissions
-    MAX_NUMBER_SUBMISSIONS = 200
+    MAX_NUMBER_SUBMISSIONS = 100
